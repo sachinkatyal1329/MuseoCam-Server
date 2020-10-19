d29640abbf04b9d5243c8e6ef868174ca56b08a7
Sachin Katyal <skatyal@seas.upenn.edu>
2020-10-19 01:13:46 -0400

    firebase

---
+++ functions/src/index.ts
@@ -0,0 +1,35 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+import * as express from 'express'
+import * as cors from 'cors'
+
+const firebaseConfig = {
+    authDomain: "pennmuseumtest.firebaseapp.com",
+    apiKey: "AIzaSyCSOcIEV7IgvCf-tfUIKRuz4yVOLnhE1co",
+    databaseURL: "https://pennmuseumtest.firebaseio.com",
+    projectId: "pennmuseumtest",
+    storageBucket: "pennmuseumtest.appspot.com",
+    messagingSenderId: "304449704558",
+    appId: "1:304449704558:web:4c741275b63ab468964c7f",
+    measurementId: "G-9EGZZT85WW"
+};
+admin.initializeApp(firebaseConfig)
+
+
+const app = express()
+app.use(cors({ origin: true}))
+
+
+app.get('/', (req, res) => {
+    res.write('hello, world')
+})
+
+exports.app = functions.https.onRequest(app);
